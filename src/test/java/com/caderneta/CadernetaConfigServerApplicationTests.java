6bebe913da15efa44c9a37487c1013dd98c2039d
siron azevedo <sirondba@gmail.com>
2021-03-01 12:16:41 -0300

    commit

---
+++ src/test/java/com/caderneta/CadernetaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caderneta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadernetaConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
